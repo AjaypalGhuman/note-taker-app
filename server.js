3dac0398747638dee383fd74131920fb7d00bda8
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-05-16 15:16:32 -0400

    created a get route and some of the post route

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const notes = require('./db/db.json');
+const allNotes = require('./db/db.json');
+const uuid = require('uuid');
 
 const app = express();
 
@@ -11,7 +12,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// used to store and retrieve notes
+app.get('/api/notes', (req, res) => {
+    res.sendFile(
+        path.join(__dirname, '/db/db.json'))
+});
 
+app.post('/api/notes', (req, res) => {
+    const allNotes = JSON.parse(
+        fs.readFileSync('./db/db.json')
+    );
+    const newNotes = req.body;
+    newNotes.id
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
