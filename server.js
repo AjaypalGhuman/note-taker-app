874428ee403f96e0611dcdf86fb22b4aa886a9d7
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-05-16 14:57:26 -0400

    added listening port

--- server.js
+++ server.js
@@ -10,3 +10,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
