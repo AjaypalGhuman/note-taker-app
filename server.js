9f9b93522e6f7825bcda84c2294255c3746e7621
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-05-16 14:47:50 -0400

    added the setup for express server

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+const notes = require('./db/db.json');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
